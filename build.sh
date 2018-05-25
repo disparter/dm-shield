f7860331894849760ce67a7e8991f82ad1ea98bb
Felipe Faria <felipe.a.b.faria@gmail.com>
2018-05-25 08:30:54 -0300

    removed invalid syntax

--- build.sh
+++ build.sh
@@ -10,8 +10,9 @@ ionic cordova build android --release
 
 # - ionic cordova build ios --device --release
 
-
-after_success:
+#sign
 jarsigner -verbose -sigalg SHA1withRSA -storepass $storepass -keypass $keypass -digestalg SHA1 -keystore dm-shield.keystore ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk dm-shield-key
 zipalign -v 4 ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk ./platforms/android/app/build/outputs/apk/release/app-release-signed.apk
+
+#supply
 supply run -j ./platforms/android/gapi.json -p com.github.disparter.dm_shield -b ./platforms/android/app/build/outputs/apk/release/app-release-signed.apk
