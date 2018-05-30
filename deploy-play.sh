5d420734731e097e184aed9d222a02becdc499ac
Felipe Faria <felipe.a.b.faria@gmail.com>
2018-05-30 14:58:09 -0300

    Changing the deploy to the deploy part to work well with the pull-request in Github.

--- deploy-play.sh
+++ deploy-play.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 cd $TRAVIS_BUILD_DIR
 
-npm install -g cordova@latest ionic
-
-# build Android and iOS packages
-ionic cordova platform remove android
-ionic cordova platform add android
-
-ionic info
-ionic cordova build android --release
-# - cordova build ios --device --release
-
 #sign
 jarsigner -verbose -sigalg SHA1withRSA -storepass $storepass -keypass $keypass -digestalg SHA1 -keystore dm-shield.keystore ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk dm-shield-key
 zipalign -v 4 ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk ./platforms/android/app/build/outputs/apk/release/app-release-signed.apk
